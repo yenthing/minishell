20d6a4e6c03c423ab8a4bd60606f6a95a8b87aa6
Yen Ng <107750559+yenthing@users.noreply.github.com>
2023-07-29 19:01:24 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,8 +12,49 @@
 
 #include "../incl/minishell.h"
 
+int get_host_name(char **av, int len)
+{
+	int result;
+	char *name;
+
+	name = av[1];
+    if (len < 1)
+        return (1);
+    result = printf("Host Name: %s", name);
+    if (result < 0 || result >= len)
+        return (1);
+    return (0);
+}
+
+char *create_prompt()
+{
+	char *str_cwd;
+	const int prompt_size = 250;
+
+	str_cwd = malloc(prompt_size + 1);
+	if (!str_cwd)
+		return (NULL);
+	getcwd(str_cwd, prompt_size);
+	//printf("$ %s", str_cwd);
+	get_host_name(&str_cwd, prompt_size);
+	printf("$ %s", str_cwd);
+	return (str_cwd);
+}
+
 void	ft_loop_minishell(t_data *data)
 {
+	(void)data;
+	char *read_cmd;
+	
+	while(1)
+	{
+		read_cmd = readline(create_prompt());
+		if (!read_cmd)
+		printf("Unable to make prompt\n");
+		continue;
+	}
+	if (!read_cmd)
+		free(read_cmd);
 }
 
 int	main(int ac, char **av, char**env)
